5c7eb3f0acb749cad8e05bc2dd2a98f2b65ad3b4
MD Sayedul Haque <mdsayedulhaque932@gmail.com>
2023-12-21 08:37:18 +0600

    project initialization

---
+++ src/Layout/MainLayout/MainLayout.jsx
@@ -0,0 +1,13 @@
+import Navbar from "../../Shared/Navbar/Navbar";
+
+
+const MainLayout = () => {
+    return (
+        <div className="max-w-7xl mx-auto">
+            <Navbar></Navbar>
+            <h1>Main Layout</h1>
+        </div>
+    );
+};
+
+export default MainLayout;
